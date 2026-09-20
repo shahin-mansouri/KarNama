73d422116a97b3d491d7faa21751031fd25735e8
Shahin <mansouri.math.kntu@gmail.com>
2026-09-20 20:24:56 +0330

    add static for collectstatic

--- my_resume/settings.py
+++ my_resume/settings.py
@@ -127,6 +127,13 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -136,3 +143,4 @@ AUTH_USER_MODEL = 'account.UserCustom'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
+

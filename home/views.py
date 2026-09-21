43f2b2524d1b86154daba95ca7ee5a310360311e
Shahin <mansouri.math.kntu@gmail.com>
2026-09-21 11:52:31 +0330

    feat: implement pagination for resume bank and update links to login

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.views.generic import TemplateView
 
 from account.models import UserCustom
@@ -24,10 +25,17 @@ class ResumeBank(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['resume_users'] = (
+        resume_users = (
             UserCustom.objects
             .filter(is_active=True, userprofile__isnull=False)
             .select_related('userprofile')
             .order_by('-date_joined')
         )
+        paginator = Paginator(resume_users, 12)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context['page_obj'] = page_obj
+        context['resume_users'] = page_obj.object_list
+        context['resume_count'] = paginator.count
         return context

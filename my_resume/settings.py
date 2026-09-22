af616d7f3a420a9d2f86634eaaaee8979dadc5c8
Shahin <mansouri.math.kntu@gmail.com>
2026-09-22 14:50:10 +0330

    fix: update STATIC_URL and streamline static file settings in settings.py

--- my_resume/settings.py
+++ my_resume/settings.py
@@ -123,15 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type

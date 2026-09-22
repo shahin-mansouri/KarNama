c62658774db6ed15c2c7656405980e1ac7aaa6cd
Shahin <mansouri.math.kntu@gmail.com>
2026-09-22 14:52:58 +0330

    feat: add CSRF_TRUSTED_ORIGINS setting for enhanced security

--- my_resume/settings.py
+++ my_resume/settings.py
@@ -18,7 +18,9 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://karnamma.ir",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/

282a746f6ef689c5f459bb467154a2174f6870f2
Shahin <mansouri.math.kntu@gmail.com>
2026-09-23 12:12:12 +0330

    feat: implement search functionality for resumes in home and resume bank pages

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator
+from django.db.models import Case, IntegerField, Q, Value, When
 from django.views.generic import TemplateView
 
 from account.models import UserCustom
@@ -17,6 +18,7 @@ class Home(TemplateView):
             .values_list('category', flat=True)
             .distinct()[:6]
         )
+        context['successful_people'] = _resume_queryset()[:3]
         return context
 
 
@@ -25,12 +27,8 @@ class ResumeBank(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        resume_users = (
-            UserCustom.objects
-            .filter(is_active=True, userprofile__isnull=False)
-            .select_related('userprofile')
-            .order_by('-date_joined')
-        )
+        query = self.request.GET.get('q', '').strip()
+        resume_users = _resume_queryset(query)
         paginator = Paginator(resume_users, 12)
         page_number = self.request.GET.get('page')
         page_obj = paginator.get_page(page_number)
@@ -38,4 +36,50 @@ class ResumeBank(TemplateView):
         context['page_obj'] = page_obj
         context['resume_users'] = page_obj.object_list
         context['resume_count'] = paginator.count
+        context['search_query'] = query
         return context
+
+
+def _resume_queryset(query=''):
+    resume_users = (
+        UserCustom.objects
+        .filter(is_active=True, userprofile__isnull=False)
+        .select_related('userprofile')
+    )
+
+    if query:
+        search_filter = (
+            Q(username__icontains=query)
+            | Q(first_name__icontains=query)
+            | Q(last_name__icontains=query)
+            | Q(userprofile__title__icontains=query)
+            | Q(userprofile__bio__icontains=query)
+            | Q(aboutme__about_me__icontains=query)
+            | Q(skill__name__icontains=query)
+            | Q(workexperience__position__icontains=query)
+            | Q(workexperience__company_name__icontains=query)
+            | Q(workexperience__description__icontains=query)
+            | Q(project__name__icontains=query)
+            | Q(project__technologies_used__icontains=query)
+            | Q(project__description__icontains=query)
+            | Q(project__field_of_activity__name__icontains=query)
+            | Q(certificate__name__icontains=query)
+            | Q(certificate__issuer__icontains=query)
+            | Q(language__name__icontains=query)
+            | Q(research__title__icontains=query)
+            | Q(research__description__icontains=query)
+            | Q(sociallink__platform__icontains=query)
+        )
+        resume_users = resume_users.filter(search_filter).distinct()
+        resume_users = resume_users.annotate(
+            relevance=(
+                Case(When(username__iexact=query, then=Value(100)), default=Value(0), output_field=IntegerField())
+                + Case(When(userprofile__title__icontains=query, then=Value(50)), default=Value(0), output_field=IntegerField())
+                + Case(When(skill__name__icontains=query, then=Value(30)), default=Value(0), output_field=IntegerField())
+                + Case(When(workexperience__position__icontains=query, then=Value(25)), default=Value(0), output_field=IntegerField())
+                + Case(When(project__name__icontains=query, then=Value(20)), default=Value(0), output_field=IntegerField())
+            )
+        )
+        return resume_users.order_by('-relevance', '-is_superuser', '-date_joined')
+
+    return resume_users.order_by('-is_superuser', '-date_joined')

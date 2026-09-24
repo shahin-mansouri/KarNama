3e3e15be00ce33adf6a47bad212fd0087d959214
Shahin <mansouri.math.kntu@gmail.com>
2026-09-24 15:29:07 +0330

    add blog generator

---
+++ home/management/commands/blog_generator.py
@@ -0,0 +1,93 @@
+import json
+import random
+import dashscope
+from django.utils.text import slugify
+from django.utils import timezone
+from home.models import BlogPost
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+dashscope.base_http_api_url = "https://dashscope-intl.aliyuncs.com/api/v1"
+
+
+class Command(BaseCommand):
+    help = "Generate a blog post using Qwen"
+
+    def handle(self, *args, **options):
+        # کد تولید مقاله اینجا
+        self.stdout.write("Blog generated successfully.")
+
+
+
+ALLOWED_CATEGORIES = (
+    "کاریابی",
+    "استخدام",
+    "ساخت رزومه",
+    "رزومه موفق",
+    "مصاحبه شغلی",
+    "مهارت‌های شغلی",
+    "برند شخصی",
+)
+
+def blog_generator():
+    # image_path = os.path.abspath(f"./downloads/{image}")
+    text=build_prompt(categorie=random.choice(ALLOWED_CATEGORIES))
+    messages = [
+    {
+        "role": "user",
+        "content": [
+            {"text": text}]
+    }]
+    response = dashscope.MultiModalConversation.call(
+        api_key=settings.DASHSCOPE_API_KEY,
+        model=settings.DASHSCOPE_MODEL,
+        messages=messages
+    )
+    return response.output.choices[0].message.content[0]["text"]
+
+
+def build_prompt(categorie):
+
+    return f"""
+                برای یک مقاله فارسی حرفه‌ای و کاربردی درباره دسته «{categorie}» بنویس.
+
+                مقاله باید مناسب SEO، غیرکلیشه‌ای، کاربردی و مخصوص افراد جویای کار و متخصصان باشد.
+                عنوان جذاب، خلاصه حداکثر 100 کاراکتر و محتوای کامل با تیترهای مناسب داشته باشد.
+
+                فقط JSON معتبر و بدون Markdown برگردان:
+
+                {{
+                    "title": "عنوان مقاله",
+                    "excerpt": "خلاصه مقاله",
+                    "content": "متن کامل مقاله",
+                    "category": "{categorie}"
+                }}
+                """.strip()
+
+
+print('start')
+blog = blog_generator()
+# print('line 74')
+# print(blog)
+blog_json = json.loads(blog)
+
+# print(
+#     json.dumps(
+#         blog_json,
+#         ensure_ascii=False,
+#         indent=4
+#     )
+# )
+
+
+BlogPost.objects.create(
+    title=blog_json["title"],
+    slug=slugify(blog_json["title"], allow_unicode=True),
+    excerpt=blog_json["excerpt"],
+    content=blog_json["content"],
+    category=blog_json["category"],
+    published_at=timezone.now(),
+    is_published=True,
+)
+
+print('published.')
